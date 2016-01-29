4b7d902ed9c3eb773cc3a1c759530b1c4cb1bc44
Adeeb Sheikh <asheikh4@u.rochester.edu>
2016-01-29 01:56:57 -0500

    Finished the basic NeuronGene class.

--- jNEAT/NeuronGene.java
+++ jNEAT/NeuronGene.java
@@ -1,5 +1,72 @@
 package jNEAT;
 
-public class NeuronGene {
+public class NeuronGene extends Gene {
 
+	public enum NeuronType {
+		
+		INPUT, HIDDEN, BIAS, OUTPUT, NONE;
+		
+	}
+	
+	private int id; //Neuron ID
+	private NeuronType type; //This is the type of neuron
+	private double tier;
+	private double p; //p in 1/(1 + e^(- a / p)), the logistic sigmoid activation function
+	private boolean recurrent; //Is this a recurrent neuron
+	
+	public NeuronGene(int geneNum, int id, NeuronType type, double tier, double p, boolean recurrent) {
+		
+		super(geneNum);
+		this.id = id;
+		this.type = type;
+		this.tier = tier;
+		this.p = p;
+		this.recurrent = recurrent;
+		
+	}
+
+	//Getters
+	public int getID(){
+		
+		return id;
+		
+	}
+	
+	public NeuronType getType(){
+		
+		return type;
+		
+	}
+	
+	public double getTier(){
+		
+		return tier;
+		
+	}
+	
+	public double getP(){
+		
+		return p;
+		
+	}
+	
+	public boolean getRecurrent(){
+		
+		return recurrent;
+		
+	}
+
+	//Setters
+	public void setP(double p){
+		
+		this.p = p;
+		
+	}
+	
+	public void setRecurrent(boolean recurrent){
+		
+		this.recurrent = recurrent;
+		
+	}
+	
 }
